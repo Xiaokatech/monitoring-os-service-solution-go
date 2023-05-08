a4dbdc1343b745338767a9d6fc50a03a22ed1abc
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-05-08 22:48:55 +0200

    Update install_windows.bat

--- install_windows.bat
+++ install_windows.bat
@@ -6,7 +6,7 @@ if not exist HelloWorldGoOsService.exe (
     goto :exit
 )
 
-sc create go-svc-HelloWorldGoOsService binpath= "%CD%\HelloWorldGoOsService.exe" start= auto DisplayName= "go-svc-HelloWorldGoOsService"
+sc create go-svc-HelloWorldGoOsService binpath= "C:\go\agentOsService\HelloWorldGoOsServiceApp.exe" start= auto DisplayName= "go-svc-HelloWorldGoOsService"
 sc description go-svc-HelloWorldGoOsService "go-svc-HelloWorldGoOsService"
 net start go-svc-HelloWorldGoOsService
 sc query go-svc-HelloWorldGoOsService
